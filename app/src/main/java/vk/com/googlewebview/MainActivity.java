df7f292dc803cf7549e8778a1d9023ce60263826
vasanth <vasanthkumar.j@outlook.com>
2019-09-17 23:44:50 +0530

    Initial commit

---
+++ app/src/main/java/vk/com/googlewebview/MainActivity.java
@@ -0,0 +1,33 @@
+package vk.com.googlewebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.google.com");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack())
+        {
+            webView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+}
